10c8be059981d475e31a937dd66696742e7deffc
unknown <petrichorwebdev@gmail.com>
2021-10-21 23:19:04 -0700

    Added stacks

--- _Computer-Science/linearDataStructures.js
+++ _Computer-Science/linearDataStructures.js
@@ -55,15 +55,17 @@ module.exports = Node;
 ////////////////////////////////////////////////////////////
 
 // LINKED LISTS
-/*
-Are comprised of nodes
-The nodes contain a link to the next node (and also the previous node for bidirectional linked lists)
-Can be unidirectional or bidirectional
-Are a basic data structure, and form the basis for many other data structures
-Have a single head node, which serves as the first node in the list
-Require some maintenance in order to add or remove nodes
-The methods we used are an example and depend on the exact use case and/or programming language being used
-*/
+{
+	/*
+	Are comprised of nodes
+	The nodes contain a link to the next node (and also the previous node for bidirectional linked lists)
+	Can be unidirectional or bidirectional
+	Are a basic data structure, and form the basis for many other data structures
+	Have a single head node, which serves as the first node in the list
+	Require some maintenance in order to add or remove nodes
+	The methods we used are an example and depend on the exact use case and/or programming language being used
+	*/
+}
 
 // LINKED LISTS in JavaScript
 class LinkedList {
@@ -185,15 +187,17 @@ function swapNodes(list, data1, data2) {
 ////////////////////////////////////////////////////////////
 
 // DOUBLY LINKED LISTS
-/*
-Are comprised of nodes that contain links to the next and previous nodes
-Are bidirectional, meaning it can be traversed in both directions
-Have a pointer to a single head node, which serves as the first node in the list
-Have a pointer to a single tail node, which serves as the last node in the list
-Require the pointers at the head of the list to be updated after addition to or removal of the head
-Require the pointers at the tail of the list to be updated after addition to or removed of the tail
-Require the pointers of the surrounding nodes to be updated after removal from the middle of the list
-*/
+{
+	/*
+	Are comprised of nodes that contain links to the next and previous nodes
+	Are bidirectional, meaning it can be traversed in both directions
+	Have a pointer to a single head node, which serves as the first node in the list
+	Have a pointer to a single tail node, which serves as the last node in the list
+	Require the pointers at the head of the list to be updated after addition to or removal of the head
+	Require the pointers at the tail of the list to be updated after addition to or removed of the tail
+	Require the pointers of the surrounding nodes to be updated after removal from the middle of the list
+	*/
+}
 
 class DoublyLinkedList {
 	constructor() {
@@ -313,25 +317,27 @@ subway.printList();
 ////////////////////////////////////////////////////////////
 
 // QUEUES
-/*
-a queue is a data structure that contains an ordered set of data that follows a 
-FIFO (first in, first out) protocol for accessing that data.
+{
+	/*
+	a queue is a data structure that contains an ordered set of data that follows a 
+	FIFO (first in, first out) protocol for accessing that data.
 
-You can visualize it as a checkout line at a supermarket:
+	You can visualize it as a checkout line at a supermarket:
 
-The customer at the front of the line (equivalent to the head in a queue) is the 
-first customer to pay for their groceries.
+	The customer at the front of the line (equivalent to the head in a queue) is the 
+	first customer to pay for their groceries.
 
-Any new customer must go to the back of the line (the tail of the queue) and wait 
-until everyone in front of them has paid for their groceries, (no line cutters allowed 
-in this supermarket!)
+	Any new customer must go to the back of the line (the tail of the queue) and wait 
+	until everyone in front of them has paid for their groceries, (no line cutters allowed 
+	in this supermarket!)
 
-The supermarket cashier only needs to check out the customer at the front of the line
+	The supermarket cashier only needs to check out the customer at the front of the line
 
-Real-life computer science applications of queues are all around us: search algorithms 
-like BFS (breadth-first search), job schedulers that run tasks on our computers, and 
-keyboard processing that interprets our keystrokes are all queue based.
-*/
+	Real-life computer science applications of queues are all around us: search algorithms 
+	like BFS (breadth-first search), job schedulers that run tasks on our computers, and 
+	keyboard processing that interprets our keystrokes are all queue based.
+	*/
+}
 
 class Queue {
 	constructor(maxSize = Infinity) {
@@ -397,5 +403,116 @@ module.exports = { load, clear };
 ////////////////////////////////////////////////////////////
 
 // STACKS
+{
+	/*
+	Stacks provide three methods for interaction:
+
+	Push - adds data to the “top” of the stack
+	Pop - returns and removes data from the “top” of the stack
+	Peek - returns data from the “top” of the stack without removing it
+
+	Stacks mimic a physical “stack” of objects. Consider a set of gym weights.
+
+	Each plate has a weight (the data). The first plate you add, or push, onto the 
+	floor is both the bottom and top of the stack. Each weight added becomes the 
+	new top of the stack.
+
+	At any point, the only weight you can remove, or pop, from the stack is the top 
+	one. You can peek and read the top weight without removing it from the stack.
+
+	The last plate that you put down becomes the first, and only, one that you 
+	can access. This is a Last In, First Out or LIFO structure. A less frequently 
+	used term is First In, Last Out, or FILO.
+	*/
+	/*
+	Stacks can be implemented using a linked list as the underlying data structure 
+	because it’s more efficient than a list or array.
+
+	Depending on the implementation, the top of the stack is equivalent to the head node 
+	of a linked list and the bottom of the stack is equivalent to the tail node.
+
+	A constraint that may be placed on a stack is its size. This is done to limit and 
+	quantify the resources the data structure will take up when it is “full”.
+
+	Attempting to push data onto an already full stack will result in a stack overflow. 
+	Similarly, if you attempt to pop data from an empty stack, it will result in a stack 
+	underflow.
+	*/
+}
+
+// Example
+
+class Stack {
+	constructor(maxSize = Infinity) {
+		this.stack = new LinkedList();
+		this.maxSize = maxSize;
+		this.size = 0;
+	}
+
+	hasRoom() {
+		return this.size < this.maxSize;
+	}
+
+	isEmpty() {
+		return this.size === 0;
+	}
+
+	push(value) {
+		if (this.hasRoom()) {
+			this.stack.addToHead(value);
+			this.size++;
+		}
+		throw new Error('Stack is full.');
+	}
+
+	pop() {
+		if (this.isEmpty()) {
+			throw new Error('Stack is empty.');
+		}
+		const value = this.stack.removeHead();
+		this.size--;
+		return value;
+	}
+
+	peek() {
+		if (this.isEmpty()) {
+			return null;
+		}
+		return this.stack.head.data;
+	}
+}
+
+module.exports = Stack;
+
+// Example
+// 1. Define an empty pizza stack with a maxSize of 6
+const pizzaStack = new Stack(6);
+
+// 2. Add pizzas as they are ready until we fill up the stack
+for (let i = 1; i <= 6; i++) {
+	pizzaStack.push(`Pizza #${i}`);
+}
+
+// 3. Try pushing another pizza to check for overflow
+try {
+	pizzaStack.push('Pizza #7');
+} catch (e) {
+	console.log(e);
+}
+
+// 4. Peek at the pizza on the top of stack and log its value
+console.log(`the first pizza to deliver is ${pizzaStack.peek()}`);
+
+// 5. Deliver all the pizzas from the top of the stack down
+for (let i = 6; i > 0; i--) {
+	pizzaStack.pop();
+}
+
+// 6. Try popping another pizza to check for empty stack
+try {
+	pizzaStack.pop();
+} catch (e) {
+	console.log(e);
+}
 
 ////////////////////////////////////////////////////////////
