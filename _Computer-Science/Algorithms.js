409a5909381117a4f37061702ca8c6b3103c9d7d
unknown <petrichorwebdev@gmail.com>
2021-11-20 19:44:38 -0800

    added misc algorithms

--- _Computer-Science/Algorithms.js
+++ _Computer-Science/Algorithms.js
@@ -183,3 +183,37 @@ const quicksort = (array, leftBound = 0, rightBound = array.length - 1) => {
 };
 
 /////////////////////////////////////////////////////////////////////
+
+// Misc Arrays
+
+// Catching Rainwater
+function catchingRainwater(heights) {
+	let totalWater = 0;
+	let leftPointer = 0;
+	let rightPointer = heights.length - 1;
+	let leftBound = 0;
+	let rightBound = 0;
+
+	while (leftPointer < rightPointer) {
+		if (heights[leftPointer] <= heights[rightPointer]) {
+			if (heights[leftPointer] > leftBound) {
+				leftBound = heights[leftPointer];
+			}
+			totalWater += leftBound - heights[leftPointer];
+			leftPointer += 1;
+		} else {
+			if (heights[rightPointer] > rightBound) {
+				rightBound = heights[rightPointer];
+			}
+			totalWater += rightBound - heights[rightPointer];
+			rightPointer -= 1;
+		}
+	}
+	return totalWater;
+}
+
+const testArray = [4, 2, 1, 3, 0, 1, 2];
+console.log(catchingRainwater(testArray));
+
+// Leave this so that we can test your code:
+module.exports = catchingRainwater;
