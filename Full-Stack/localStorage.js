d8964ed3c5e1c85d7ace0daaa2be39c0795c80b7
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2021-12-04 00:25:52 -0800

    finished express & routers

---
+++ Full-Stack/localStorage.js
@@ -0,0 +1,10 @@
+// Local Storage basics and examples
+
+localStorage.setItem('I', 'Like Turtles');
+localStorage.setItem('age', 22);
+
+localStorage.getItem('age');
+
+localStorage.removeItem('I');
+
+localStorage.clear();
