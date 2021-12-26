dc8e3732dde379f5aa96e6b954f64fd967eed8bb
unknown <petrichorwebdev@gmail.com>
2021-12-26 11:09:00 -0800

    updated cpp

--- C++/basics.cpp
+++ C++/basics.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <iostream> // import the library to use input and output
 
-int main() {
-    std::cout << "Hello World\n";
-    return 0;
+int main() { 
+    // every cpp file needs a main function, usually an 'int' function that returns 0 when it works properly.
+    std::cout << "What's up nerds\n";
+    return 0; // finish the function
 }
