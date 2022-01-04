b22f352eb86a471a809d3daf684be4375faf71be
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-01-03 21:41:29 -0800

    changed some C++ data

--- C++/basics.cpp
+++ C++/basics.cpp
@@ -9,55 +9,56 @@ everything else uses lowercase with underscores (variable_name)
 // Using namespace std // optional to avoid using std:: before certain
 // declarations
 
-int main() {  // main function required, always returns an int, 0, if it ran
-              // properly
+int main()
+{ // main function required, always returns an int, 0, if it ran
+  // properly
 
-  // print the string 'Hello World'
-  std::cout << "Hello World\n";
+    // print the string 'Hello World'
+    std::cout << "Hello World\n";
 
-  // declare a variable of type 'int'
-  int input;
+    // declare a variable of type 'int'
+    int input;
 
-  // receive user input and store it in a variable
-  std::cin >> input;
+    // receive user input and store it in a variable
+    std::cin >> input;
 
-  /*
-  Common data types:
-    int
-    double
-    char
-    std::string
-    bool
-    array
-    std::vector<type> // requires #include <vector>
-  */
+    /*
+    Common data types:
+      int
+      double
+      char
+      std::string
+      bool
+      array
+      std::vector<type> // requires #include <vector>
+    */
 
-  /*
-  Unique operators:
-    //pre-increment
-    --variable_name
-    ++variable_name
-    // this method assigns the new value to the original variable regardless of
-  context
+    /*
+    Unique operators:
+      //pre-increment
+      --variable_name
+      ++variable_name
+      // this method assigns the new value to the original variable regardless of
+    context
 
-    //post-increment
-    variable_name--
-    variable_name++
-    // this method does not assign the value to the original,
-    ex.
-    int old_var = 4;
-    int new_var = old_var-- // new_var == 3 && old_var == 4
+      //post-increment
+      variable_name--
+      variable_name++
+      // this method does not assign the value to the original,
+      ex.
+      int old_var = 4;
+      int new_var = old_var-- // new_var == 3 && old_var == 4
 
 
-    Bitwise operators:
-    <<
-    >>
-    ~
-    &
-    |
-    ^
-  */
+      Bitwise operators:
+      <<
+      >>
+      ~
+      &
+      |
+      ^
+    */
 
-  // end the program
-  return 0;
+    // end the program
+    return 0;
 }
