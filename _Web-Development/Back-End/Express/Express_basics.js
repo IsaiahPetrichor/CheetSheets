ca032a9e7e325ed1de55709740e45e0d23505d1b
Isaiah Petrichor <30422715+IsaiahPetrichor@users.noreply.github.com>
2022-01-12 19:54:24 -0800

    updated express basics to be concise and more in depth

--- _Web-Development/Back-End/Express/Express_basics.js
+++ _Web-Development/Back-End/Express/Express_basics.js
@@ -1,6 +1,8 @@
+// require in express and initialize it
 const express = require('express');
 const app = express();
 
+// pull in random JS utitlity functions
 const {
 	getElementById,
 	getIndexById,
@@ -9,21 +11,46 @@ const {
 	createElement,
 } = require('./_utility-functions/utils');
 
+// declare port for the server
 const PORT = process.env.PORT || 4001;
-// Use static server to serve the Express Yourself Website
+/* 
+Use static server to serve the Express Yourself Website
+express.static tells express to serve static 
+assets from specified directory. It can take an optional
+parameter that is an object holding options for how
+to serve the assests.
+*/
 app.use(express.static('public'));
 
+// create an array to hold sample data
 const expressions = [];
 seedElements(expressions, 'expressions');
-const animals = [];
-seedElements(animals, 'animals');
 
-// Get all expressions
+// Request paths can use all sorts of options
+/*
+Basic paths
+	'/abc' matches ALL paths that start with it
+Path Patterns
+	'/ab?c' matches ALL paths that start with '/ab' OR '/abc'
+	'/ab+c' matches ALL paths starting with '/abc', '/abbc', '/abbbbbbbc', etc.
+	'/ab*c' matches ALL paths starting with /abc, /abxc, /abFOOc, etc.
+	'/a(bc)?d matches paths starting with /ad AND /abcd
+Regex
+	works like normal regular expressions within the path parameter
+Arrays
+	works in combination with any other path type.
+	['/abc', /\/lmn|\/pqr/] matches paths starting with /abc, /lmn, and /pqr
+*/
+
+// GET request. Gets all expressions
 app.get('/expressions', (req, res, next) => {
 	res.send(expressions);
 });
 
 // Get a single expression
+/* 
+the ':' character tells express to put the following into the req.params object
+*/
 app.get('/expressions/:id', (req, res, next) => {
 	const foundExpression = getElementById(req.params.id, expressions);
 	if (foundExpression) {
@@ -33,7 +60,7 @@ app.get('/expressions/:id', (req, res, next) => {
 	}
 });
 
-// Update an expression
+// PUT request. Updates an expression
 app.put('/expressions/:id', (req, res, next) => {
 	const expressionIndex = getIndexById(req.params.id, expressions);
 	if (expressionIndex !== -1) {
@@ -44,7 +71,7 @@ app.put('/expressions/:id', (req, res, next) => {
 	}
 });
 
-// Create an expression
+// POST request. Creates an expression
 app.post('/expressions', (req, res, next) => {
 	const receivedExpression = createElement('expressions', req.query);
 	if (receivedExpression) {
@@ -55,7 +82,7 @@ app.post('/expressions', (req, res, next) => {
 	}
 });
 
-// Delete an expression
+// DELETE request. Deletes an expression
 app.delete('/expressions/:id', (req, res, next) => {
 	const expressionIndex = getIndexById(req.params.id, expressions);
 	if (expressionIndex !== -1) {
@@ -66,54 +93,7 @@ app.delete('/expressions/:id', (req, res, next) => {
 	}
 });
 
-// Get all animals
-app.get('/animals', (req, res, next) => {
-	res.send(animals);
-});
-
-// Get a single animal
-app.get('/animals/:id', (req, res, next) => {
-	const animal = getElementById(req.params.id, animals);
-	if (animal) {
-		res.send(animal);
-	} else {
-		res.status(404).send();
-	}
-});
-
-// Create an animal
-app.post('/animals', (req, res, next) => {
-	const receivedAnimal = createElement('animals', req.query);
-	if (receivedAnimal) {
-		animals.push(receivedAnimal);
-		res.status(201).send(receivedAnimal);
-	} else {
-		res.status(400).send();
-	}
-});
-
-// Update an animal
-app.put('/animals/:id', (req, res, next) => {
-	const animalIndex = getIndexById(req.params.id, animals);
-	if (animalIndex !== -1) {
-		updateElement(req.params.id, req.query, animals);
-		res.send(animals[animalIndex]);
-	} else {
-		res.status(404).send();
-	}
-});
-
-// Delete a single animal
-app.delete('/animals/:id', (req, res, next) => {
-	const animalIndex = getIndexById(req.params.id, animals);
-	if (animalIndex !== -1) {
-		animals.splice(animalIndex, 1);
-		res.status(204).send();
-	} else {
-		res.status(404).send();
-	}
-});
-
+// Start the server listening on the above defined port.
 app.listen(PORT, () => {
 	console.log(`Server is listening on ${PORT}`);
 });
