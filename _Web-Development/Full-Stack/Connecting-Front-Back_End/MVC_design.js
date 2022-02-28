d696c745fcd1bf021a85a81740a141480598e176
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-02-28 15:04:55 -0800

    Renamed MVC_model to MVC_design

---
+++ _Web-Development/Full-Stack/Connecting-Front-Back_End/MVC_design.js
@@ -0,0 +1,17 @@
+/* 
+The model view controller design pattern is the most common way to 
+structure applications that need to have visual interface and 
+data storage.
+*/
+
+// Model
+{
+}
+
+// View
+{
+}
+
+// Controller
+{
+}
