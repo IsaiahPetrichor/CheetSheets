cf6d209364f9d06e7b3fbf2184dbb109f96e2044
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-09-10 20:33:00 -0700

    finished functions and lambdas

---
+++ _Languages/Python/functions.py
@@ -0,0 +1,8 @@
+# define a function
+def functionName(PARAMETER):
+    pass
+
+# Lambdas are anonymous functions
+# like JS anonymous callback functions
+x = lambda parameter : parameter + 4
+print(x(1)) # prints 5
