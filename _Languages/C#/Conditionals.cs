a2343d73f5802f20e98c33f38963cbcae2328fa5
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-09-30 21:16:08 -0700

    Learned C# String and Started Conditionals

---
+++ _Languages/C#/Conditionals.cs
@@ -0,0 +1,19 @@
+namespace c_sharp
+{
+    internal static class Conditionals
+    {
+        public static void GetConditionals()
+        {
+            int x = 0;
+            int y = 4;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater that y")
+            }
+            else if (y > x)
+            {
+                // do something else
+            }
+        }
+    }
+}
--- _Languages/C#/Strings.cs
+++ _Languages/C#/Strings.cs
@@ -4,6 +4,27 @@
     {
         public static void GetStrings()
         {
+            string myString = "Hello mate!";
+            string secondStr = "I like turtles.";
+            // String length
+            Console.WriteLine(myString.Length);
+            Console.WriteLine(secondStr.ToUpper());
+            Console.WriteLine(myString.ToLower());
+
+            // Concatenation
+            Console.WriteLine(myString + " " + secondStr));
+            Console.WriteLine(string.Concat(myString, " I eat rice often!");
+
+            // Interpolation
+            Console.WriteLine($"{myString} Did you know that {secondStr}");
+
+            // String Access
+            Console.WriteLine(myString[0]); // 'H'
+            Console.WriteLine(myString.IndexOf("m")); // index: 6
+            Console.WriteLine(secondStr.Substring(7)); // "turtles"
+
+            // Escape characters
+            Console.WriteLine("\\ \" \' \n \t \b"); // \n new line \t tab \b backspace
         }
     }
 }
